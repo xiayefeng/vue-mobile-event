af910c850f378331936e7d76ec8c1b167613b893
xiayefeng <xiayu_12@yeah.net>
2020-06-15 16:07:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ export function inserted (el, binding, vNode) {
   }
   if (typeof binding.value === 'function') {
     myEvent[binding.arg](binding.value)
-  }else if(typeof binding.value === 'object' && binding.value && Reflect.has(binding.value, 'startHandle') && Reflect.has(binding.value, 'endHandle') && typeof binding.value.startHandle === 'function' && typeof binding.value.endHandle === 'function' && binding.arg === 'longTap') {
+  } else if (typeof binding.value === 'object' && binding.value && Reflect.has(binding.value, 'startHandle') && Reflect.has(binding.value, 'endHandle') && typeof binding.value.startHandle === 'function' && typeof binding.value.endHandle === 'function' && binding.arg === 'longTap') {
       myEvent[binding.arg](binding.value.startHandle, binding.value.endHandle)
   } else {
     throw new TypeError(`expression must be function or a object, object has 'startHandle' and 'endHandle' function`)
